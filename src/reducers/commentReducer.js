6ad27315f67ef395f36d2f763788b820a60c4ba0
Sydney James <sydneyjames7@gmail.com>
2021-02-18 16:42:49 -0800

    i think commentreducer is good

---
+++ src/reducers/commentReducer.js
@@ -0,0 +1,28 @@
+import { SET_COMMENT, DELETE_COMMENT } from '../actions/commentActions';
+
+const initialState = {
+  comments: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case SET_COMMENT:
+      return {
+        ...state,
+        comments: [...state.comments, action.payload]
+      };
+    case DELETE_COMMENT: {
+      const comments = state
+        .comments.filter(comment => comment.index !== action.payload);
+
+      return {
+        ...state,
+        comments
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
