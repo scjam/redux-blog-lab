3585cedaa58c6e9ea91ea8b62b369bb2e4573c60
Sydney James <sydneyjames7@gmail.com>
2021-02-18 18:26:50 -0800

    test passing

--- src/selectors/postSelector.test.js
+++ src/selectors/postSelector.test.js
@@ -4,9 +4,10 @@ import { getPosts } from './postSelector';
 describe('post selectors', () => {
   it('should select your posts from state', () => {
     const state = {
-      posts: [{ title: 'hello world', content: 'bonjour la monde' }]
+      posts: {
+        posts: [{ title: 'hello world', content: 'bonjour la monde' }]
+      }
     };
-
     const posts = getPosts(state);
     expect(posts).toEqual([{ title: 'hello world', content: 'bonjour la monde' }]);
   });
